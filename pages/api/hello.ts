3815a6f236f1877b94406ce8b0cbcda192e83d90
MdTehseenKhan <tehseenk435@gmail.com>
2023-07-05 14:02:57 +0500

    first commit

---
+++ pages/api/hello.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default async function hello(req: NextApiRequest, res: NextApiResponse ) {
+  res.status(200).json({
+    message: "Hello World!"
+  })
+}
